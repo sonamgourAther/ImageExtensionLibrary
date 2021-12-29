b25247f52dcdd68b999996e1683c4851923f181b
sonamgourAther <sonam.gour@atherenergy.com>
2021-12-29 19:06:28 +0530

    UPDATE :: first commit after all setup

--- Example/ImageExtensionLibrary/ViewController.swift
+++ Example/ImageExtensionLibrary/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import ImageExtensionLibrary
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var img: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        img.circleImageView(borderColor: UIColor.red, borderWidth: 2.0)
     }
 
     override func didReceiveMemoryWarning() {
--- ImageExtensionLibrary.podspec
+++ ImageExtensionLibrary.podspec
@@ -31,6 +31,7 @@ TODO: Add long description of the pod here.
   s.ios.deployment_target = '9.0'
 
   s.source_files = 'ImageExtensionLibrary/Classes/**/*'
+  s.frameworks = 'UIKit'
   
   # s.resource_bundles = {
   #   'ImageExtensionLibrary' => ['ImageExtensionLibrary/Assets/*.png']
